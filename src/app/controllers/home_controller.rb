40fffa21a0525eb7111f6767e3ede39004ffbdc4
Jabili Gadde <gaddej1@tcnj.edu>
2022-03-27 19:24:46 -0400

    Adds calculate how long before homeless use case and adds styling

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -17,5 +17,59 @@ class HomeController < ApplicationController
   # Also, any local variable made inside this method will also exist
   # in the view to be displayed. Do not look up information within the
   # view itself. Do it here, please!
-  def index; end
+
+  # <%= form_for :anything, url: "controllers/home_controller/index" do |form| %>
+  #   <%= form.label :monthly_necessary_expenses %>
+  #   <%= form.number_field :monthly_necessary_expenses, step: :any %>
+  
+  #   <%= form.label :savings %>
+  #   <%= form.number_field :savings, step: :any %>
+  #   <%= form.submit "Submit" %>
+  # <% end  %>
+  
+  def index
+    
+  end
+
+  def calculate
+    # @request = request.raw_post
+    @expenses = params["monthly_necessary_expenses"].to_f
+    @savings = params["savings"].to_f
+
+    @months = @savings/@expenses
+
+    @statement = ""
+
+    if @months < 1
+      @weeks = @months * 4
+      if @weeks >= 1
+        if @weeks.to_i == 1
+          @statement = @weeks.to_i.to_s + " week"
+        else
+          @statement = @weeks.to_i.to_s + " weeks"
+        end
+      else
+        @days = @months * 30
+        if @days.to_i == 1
+          @statement = @days.to_i.to_s + " day"
+        else
+          @statement = @days.to_i.to_s + " days"
+        end
+      end
+    elsif @months >= 12
+      @years = @months / 12
+      if @years.to_i == 1
+        @statement = @years.to_i.to_s + " year"
+      else
+        @statement = @years.to_i.to_s + " years"
+      end
+    else
+      if @months.to_i == 1
+        @statement = @months.to_i.to_s + " month"
+      else
+        @statement = @months.to_i.to_s + " months"
+      end
+    end
+  end
+
 end
